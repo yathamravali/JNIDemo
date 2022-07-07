ad2551902a85e538af4d8d22c8b6bce821307819
Ravali Yatham <46520016+yathamravali@users.noreply.github.com>
2022-07-07 18:28:38 +0530

    Update Crash.c

--- Crash.c
+++ Crash.c
@@ -1,8 +1,10 @@
 #include "Crash.h"
 #include "jni.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 JNIEXPORT void JNICALL Java_Crash_printHello(JNIEnv *env, jobject obj){
-	char name=(char*)malloc(8);
+	char *name=(char *)malloc(8);
 	strcpy(name,"Ravali Yatham");
 	return;
 }
